29a0a42b6a7bf155b4c2036f4d9386052f6429b0
Tomas Heguy <tomasheguy@Tomass-MacBook-Air.local>
2021-10-04 23:41:37 -0300

    despues de la clase 14

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ const path = require('path');
 
 app.use(express.static('public')); // punto 4, para poder acceder desde el browser a todos los archivos 
 
+app.use(express.urlencoded());
+
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 });
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+
+app.post('/register', (req,res) =>{
+    console.log(req.body); // para leer el body
+    res.redirect('/');
+})
+
+app.post('/login', (req,res) =>{
+    console.log(req.body); // para leer el body
+    res.redirect('/');
+})
 
 app.listen(3030, () => console.log('levantaste el server con exito'));
