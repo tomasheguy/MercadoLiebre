20fe72dc056136def1ee2c07900e6bf5df1d9a6b
Tomas Heguy <tomasheguy@Tomass-MacBook-Air.local>
2021-10-06 18:53:13 -0300

    pre clase 15

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.get('/login', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
 });
 
+app.post('/search', (req,res) =>{
+    console.log(req.body); // para leer el body
+    res.redirect('/');
+})
+
 app.post('/register', (req,res) =>{
     console.log(req.body); // para leer el body
     res.redirect('/');
@@ -27,5 +32,6 @@ app.post('/login', (req,res) =>{
     console.log(req.body); // para leer el body
     res.redirect('/');
 })
+
 
 app.listen(3030, () => console.log('levantaste el server con exito'));
